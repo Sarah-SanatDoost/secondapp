e387b33c581ee8c3a07160593ddce7db56871eda
sara <sanatdoosts@gmail.com>
2022-08-31 11:19:44 +0430

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'secondapp';
+ username :string = ''
+
+ public onUserNameInputChanged (e: Event){
+  this.username = (<HTMLInputElement>e.target).value;
+ }
 }
